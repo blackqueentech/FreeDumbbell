26f6333cb5f735c57f1891f32c7cff8609689369
blackqueentech <dellaanjeh@Dellas-MacBook-Pro.local>
2017-01-09 20:00:38 -0800

    catching up

---
+++ app/src/main/java/com/example/dellaanjeh/freedumbbell/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.dellaanjeh.freedumbbell;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.etEmail) EditText etEmail;
+    @BindView(R.id.etPassword) EditText etPassword;
+    @BindView(R.id.btnLogin) Button btnLogin;
+    @BindView(R.id.btnGoogleLogin) Button btnGoogleLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+
+    }
+
+}
